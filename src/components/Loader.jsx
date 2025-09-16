c6b70691f6880be4a67820af05ddd3c6a9ba382e
Ahmedl-Be <ahmedabdallabeltagy@gmail.com>
2025-09-16 02:59:50 +0200

    Recent Payments api paganation && delete subs and payments && sidebar fixed with grid

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,22 +4,25 @@ import { Wifi } from "lucide-react";
 const Loader = () => {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-blue-800 z-50">
-      {/* Animated Wifi Icon */}
-      <motion.div
-        initial={{ rotate: -45 }}
-        animate={{
-          color: ["#ffffff", "#D9F266", "#ffffff"], // white -> green -> white
-        }}
-        transition={{
-          duration: 2,
-          repeat: Infinity,
-          ease: "easeInOut",
-        }}
-      >
-        <Wifi size={80} />
-      </motion.div>
 
-      {/* Business tagline */}
+      <div className="flex items-center gap-3">
+        <motion.div
+          initial={{ rotate: -45 }}
+          animate={{
+            color: ["#ffffff", "#D9F266", "#ffffff"],
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+        >
+          <Wifi size={60} />
+        </motion.div>
+
+        <h1 className="text-white text-2xl font-bold">Elite UK</h1>
+      </div>
+
       <motion.p
         className="mt-6 text-white text-lg font-semibold"
         initial={{ opacity: 0 }}
